6451307ab0389fcbc9e97cc8d29cf7fd1ddec46a
Setup boilerplate <setup-boilerplate@example.com>
2024-02-02 17:10:04 +0000

    Initialize boilerplate fullstack-boilerplate-dotnet-angular

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   env: {
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:5000',
   },
   e2e: {
     setupNodeEvents(on, config) {},
     supportFile: false,
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4200',
   },
 })
