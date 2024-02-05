b6ac950380158eb929876ab0ecc7a3ad4c0ae3d3
Adam Risberg <risbergadam@gmail.com>
2024-02-05 10:27:29 +0100

    Thanks alvalabs for having quote sensitive apiUrl parsing code.

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   env: {
-    apiUrl: "http://localhost:5000",
+    apiUrl: 'http://localhost:5000',
   },
   e2e: {
     setupNodeEvents(on, config) {},
     supportFile: false,
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
   },
 });
