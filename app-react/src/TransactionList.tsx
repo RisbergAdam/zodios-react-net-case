a2eb187165c49b0a871f73a9b5d860e796b2c442
Adam Risberg <risbergadam@gmail.com>
2024-02-05 10:12:30 +0100

    Better error & loading states.

--- app-react/src/TransactionList.tsx
+++ app-react/src/TransactionList.tsx
@@ -11,8 +11,9 @@ const TransactionList = () => {
     return (
       <div>
         <p className="mb-2">
-          <b>Transaction history loading</b>
+          <b>Transaction history</b>
         </p>
+        <progress className="progress is-primary" />
       </div>
     );
   }
@@ -21,58 +22,75 @@ const TransactionList = () => {
     return (
       <div>
         <p className="mb-2">
-          <b>Transaction history error</b>
+          <b>Transaction history</b>
         </p>
+        <div className="notification is-danger is-light">
+          Could not fetch transactions, please try again later.
+        </div>
       </div>
     );
   }
+
+  const hasTransactions = transactionsQuery.data.length > 0;
 
   return (
     <div>
       <p className="mb-2">
         <b>Transaction history</b>
       </p>
-      <nav className="panel">
-        {transactionsQuery.data.map((tx, i) => {
-          const isFirst = i === 0;
-          const account = accountsQuery.data.find(
-            (acc) => acc.account_id === tx.account_id
-          );
 
-          return (
-            <div
-              className="panel-block is-flex-wrap-wrap"
-              data-type="transaction"
-              data-account-id={tx.account_id}
-              data-amount={tx.amount}
-              data-balance={account?.balance ?? 0}
-              key={tx.transaction_id}
-            >
-              <p>
-                Account: <b className="is-family-monospace">{tx.account_id}</b>
-              </p>
+      {!hasTransactions && (
+        <>
+          <div className="panel-block">
+            <p className="has-text-grey-light">No transactions.</p>
+          </div>
+        </>
+      )}
 
-              <p className="ml-auto">
-                <b className="is-family-monospace">
-                  {formatCurrency(tx.amount)}
-                </b>
-              </p>
+      {hasTransactions && (
+        <nav className="panel">
+          {transactionsQuery.data.map((tx, i) => {
+            const isFirst = i === 0;
+            const account = accountsQuery.data.find(
+              (acc) => acc.account_id === tx.account_id
+            );
 
-              {isFirst && account && (
-                <>
-                  <div style={{ flexBasis: "100% " }} />
-                  <p className="has-text-info">
-                    Current account balance:{" "}
-                    <b className="is-family-monospace">
-                      {formatCurrency(account.balance)}
-                    </b>
-                  </p>
-                </>
-              )}
-            </div>
-          );
-        })}
-      </nav>
+            return (
+              <div
+                className="panel-block is-flex-wrap-wrap"
+                data-type="transaction"
+                data-account-id={tx.account_id}
+                data-amount={tx.amount}
+                data-balance={account?.balance ?? 0}
+                key={tx.transaction_id}
+              >
+                <p>
+                  Account:{" "}
+                  <b className="is-family-monospace">{tx.account_id}</b>
+                </p>
+
+                <p className="ml-auto">
+                  <b className="is-family-monospace">
+                    {formatCurrency(tx.amount)}
+                  </b>
+                </p>
+
+                {isFirst && account && (
+                  <>
+                    <div style={{ flexBasis: "100% " }} />
+                    <p className="has-text-info">
+                      Current account balance:{" "}
+                      <b className="is-family-monospace">
+                        {formatCurrency(account.balance)}
+                      </b>
+                    </p>
+                  </>
+                )}
+              </div>
+            );
+          })}
+        </nav>
+      )}
     </div>
   );
 };
